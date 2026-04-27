2e1d0155b716125554f102e0dc534960cb34e5d4
kitokoh <kitokoh.africa@gmail.com>
2026-04-27 07:32:28 +0300

    fix(api): remove duplicated down signature

--- api/database/migrations/public/2026_04_24_000015_create_languages_table.php
+++ api/database/migrations/public/2026_04_24_000015_create_languages_table.php
@@ -36,7 +36,6 @@ return new class extends Migration {
     }
 
     public function down(): void
-    public function down(): void
     {
         DB::statement('SET search_path TO public');
         Schema::dropIfExists('languages');
