0e9acc91d544c3434087db656a399542bf70fc8e
kitokoh <kitokoh.africa@gmail.com>
2026-05-08 02:55:06 +0300

    fix(api): use untyped withinTransaction override

--- api/database/migrations/public/2026_05_02_000003_create_company_requests_table.php
+++ api/database/migrations/public/2026_05_02_000003_create_company_requests_table.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    public bool $withinTransaction = false;
+    public $withinTransaction = false;
 
     public function up(): void
     {
