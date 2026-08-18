20aa3a7e292144ee93d4637114376f6d3e650969
kitokoh <7.2901455e+07+kitokoh@users.noreply.github.com>
2026-08-18 20:56:00 +0000

    fix(mobile): import legacy state provider

--- front/mobile_apps/leopardo_platform_admin/lib/src/features/support/support_tickets_screen.dart
+++ front/mobile_apps/leopardo_platform_admin/lib/src/features/support/support_tickets_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod/legacy.dart';
 import 'package:go_router/go_router.dart';
 import 'package:leopardo_core/core/theme/app_colors.dart';
 import 'package:leopardo_core/core/widgets/leopardo_badge.dart';
