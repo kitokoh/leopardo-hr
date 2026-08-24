9694fddea93fcefe3573fb086f8f9265c44304cc
kitokoh <7.2901455e+07+kitokoh@users.noreply.github.com>
2026-08-24 11:25:55 +0000

    fix: re-export shared mobile core providers

--- front/mobile_apps/leopardo_employee/lib/core/providers/core_providers.dart
+++ front/mobile_apps/leopardo_employee/lib/core/providers/core_providers.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+export 'package:leopardo_core/core/providers/core_providers.dart';
+
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
