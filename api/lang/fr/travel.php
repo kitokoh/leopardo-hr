5a8f25c10c75c2a807c833da3212fff44fc2d7f7
kitokoh <kitokoh@users.noreply.github.com>
2026-08-30 11:16:41 -0400

    feat(travel): i18n fr/en/ar/tr (catalogue travel.*) + collection Postman TravelAgency (TRAVEL-1009/#6122, TRAVEL-1006/#6119)

---
+++ api/lang/fr/travel.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'notifications' => [
+        'booking_confirmed' => 'Réservation confirmée :reference',
+        'booking_cancelled' => 'Réservation annulée :reference',
+        'ticket_issued' => 'Billet émis :ticket',
+        'reminder_title' => 'Rappel de voyage',
+        'reminder_body' => 'Votre trajet :code part à :time.',
+    ],
+    'reports' => [
+        'export_job_queued' => 'Export programmé',
+        'export_job_failed' => 'Export en échec',
+    ],
+    'content' => [
+        'article_published' => 'Article publié',
+        'article_flagged' => 'Article signalé pour modération',
+        'comment_pending' => 'Commentaire en attente de modération',
+    ],
+];
